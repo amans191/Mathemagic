57143787aa63f49a87dd56e314045d02e055d76d
amandeepsingh <amans191@gmail.com>
2018-03-10 12:19:18 +0000

    Colour is now changing

--- src/pages/StudentTabs/StudentTabs.ts
+++ src/pages/StudentTabs/StudentTabs.ts
@@ -4,19 +4,22 @@ import { StudentHomePage} from '../StudentHome/StudentHome';
 import { StudentVideosPage } from "../StudentVideos/StudentVideos";
 import { StudentprofilePage } from "../studentprofile/studentprofile";
 import { StudentSettignsPage } from "../StudentSettigns/StudentSettigns";
+import {SettingsProvider} from "../../providers/settings/settings";
 
 @Component({
   templateUrl: 'StudentTabs.html'
 })
 export class StudentTabsPage {
 
+  selectedTheme: String;
+
   tab1Root = StudentHomePage;
   tab2Root = StudentVideosPage;
   tab3Root = StudentprofilePage;
   tab4Root = StudentSettignsPage;
 
-  constructor() {
-
+  constructor(private settings: SettingsProvider) {
+    this.settings.getActiveTheme().subscribe(val => this.selectedTheme = val);
   }
 
 }
